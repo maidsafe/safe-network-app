f220369dc709020258b33f3a84a3de61898d672e
Josh Wilson <joshuef@gmail.com>
2019-11-14 16:11:41 +0100

    WIP test fixing

--- __e2e__/Permissions/Request.e2e.ts
+++ __e2e__/Permissions/Request.e2e.ts
@@ -2,7 +2,8 @@ import { Selector } from 'testcafe';
 import { waitForReact } from 'testcafe-react-selectors';
 import { clickOnMainMenuItem } from 'testcafe-browser-provider-electron';
 import { getPageUrl, getPageTitle, getByAria } from '../helpers';
-import { isCI } from '$Constants';
+
+const isCI = !!process.env.CI;
 
 const assertNoConsoleErrors = async ( t ): Promise<void> => {
     const { error } = await t.getBrowserConsoleMessages();
